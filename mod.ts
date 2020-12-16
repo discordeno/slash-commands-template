8d46d6db88c903be7a1129d4d20c31a1972c629d
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-12-16 19:53:35 +0000

    add base mod.ts

---
+++ mod.ts
@@ -0,0 +1,31 @@
+import { startServer, InteractionResponseType } from "./deps.ts";
+
+startServer({
+    publicKey: "PUBLIC_KEY_HERE_FROM_YOUR_BOT_DASHBOARD",
+    // CHANGE THIS IF YOU DID NOT USE THE DEFAULT 80 AS ngrok said
+    port: 80,
+    // YOUR CUSTOM HANDLER
+    handleApplicationCommand(payload) {
+      // Handle the command
+      if (payload.data?.name === "ping") {
+        return {
+          status: 200,
+          body: {
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: { content: "Pong! Discordeno best lib!" },
+          },
+        };
+      }
+
+      return {
+        status: 200,
+        body: {
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content:
+              "Whoopsies! Seems the handling for this command is missing. Please contact my developers!",
+          },
+        },
+      };
+    }
+})
