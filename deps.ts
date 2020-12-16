9dbd28383b3b9010a0e9242d53d5e994ebc40564
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-12-16 19:53:29 +0000

    add deps

---
+++ deps.ts
@@ -0,0 +1,2 @@
+export * from "https://raw.githubusercontent.com/discordeno/discordeno/slash-cmds/src/interactions/mod.ts";
+export * from "https://raw.githubusercontent.com/discordeno/discordeno/slash-cmds/src/interactions/types/mod.ts";
