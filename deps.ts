eba7f5725a93b8e39a96469cd93780207eae16a5
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-12-17 02:42:18 +0000

    more deps for later

--- deps.ts
+++ deps.ts
@@ -1,2 +1,5 @@
 export * from "https://raw.githubusercontent.com/discordeno/discordeno/slash-cmds/src/interactions/mod.ts";
 export * from "https://raw.githubusercontent.com/discordeno/discordeno/slash-cmds/src/interactions/types/mod.ts";
+export * from "https://deno.land/std@0.81.0/fmt/colors.ts";
+export * from "https://deno.land/std@0.81.0/async/delay.ts";
+export * from "./cache.ts";
