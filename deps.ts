cc94f8c076df9fa741c90be55f888b9322185d37
Ayyan <ayyantee@gmail.com>
2020-12-29 12:37:10 +0400

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export * from "https://raw.githubusercontent.com/discordeno/discordeno/master/src/interactions/mod.ts";
 export * from "https://deno.land/std@0.81.0/fmt/colors.ts";
 export * from "https://deno.land/std@0.81.0/async/delay.ts";
-export * from "./utils/cache.ts";
+export * from "./util/cache.ts";
