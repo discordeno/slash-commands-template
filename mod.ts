b497d22ffca754f9bdfe20567295c7f320ac052a
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-02-26 18:04:10 +0000

    remove moer deno ns

--- mod.ts
+++ mod.ts
@@ -1,15 +1,11 @@
 import { botCache } from "./util/cache.ts";
-import { importDirectory } from "./util/helpers.ts";
 
-await importDirectory(Deno.realPathSync("./commands"));
 console.log(`Loaded ${botCache.commands.size} command(s).`);
 
 // TODO: implement arguments
-// await importDirectory(Deno.realPathSync("./arguments"));
 console.log(`Loaded ${botCache.arguments.size} custom argument(s).`);
 
 // TODO: implement inhibitors
-// await importDirectory(Deno.realPathSync("./inhibitors"));
 console.log(`Loaded ${botCache.inhibitors.size} inhibitor(s).`);
 
 // This fetch process makes sure we can avoid post/creating dupes on startup
